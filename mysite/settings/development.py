0517192324cd6b222a31f08cfb97a04175ef6b80
mppatterson84 <mppatterson84@gmail.com>
2021-04-03 09:03:36 -0500

    start Django project and edit settings

---
+++ mysite/settings/development.py
@@ -0,0 +1,8 @@
+from .base import *
+
+ALLOWED_HOSTS = ['*']
+
+# SSL/HTTPS
+# https://docs.djangoproject.com/en/3.1/topics/security/#ssl-https
+
+SECURE_SSL_REDIRECT = False
---
+++ mysite/settings/production.py
@@ -0,0 +1,8 @@
+from .base import *
+
+ALLOWED_HOSTS = ['https://www.michael-patterson.com']
+
+# SSL/HTTPS
+# https://docs.djangoproject.com/en/3.1/topics/security/#ssl-https
+
+SECURE_SSL_REDIRECT = True
