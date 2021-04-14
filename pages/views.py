e86bc9037bccf576717a93b663dfa70ad8891198
mppatterson84 <mppatterson84@gmail.com>
2021-04-13 19:42:04 -0500

    update title

--- pages/views.py
+++ pages/views.py
@@ -9,7 +9,7 @@ class HomePageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Home'
+        # context['title'] = 'Home'
         context['home_active'] = 'active'
         context['home_active_link'] = '#'
         context['home_active_sr'] = '<span class="sr-only">(current)</span>'
