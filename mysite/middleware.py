89aebc17bb4caa970d50a6cd0d616137bdef2ab8
mppatterson84 <mppatterson84@gmail.com>
2021-04-15 12:26:16 -0500

    Update middleware.py

--- mysite/middleware.py
+++ mysite/middleware.py
@@ -9,7 +9,7 @@ class ExtraHttpHeaders:
 
         response = self.get_response(request)
         response['Strict-Transport-Security'] = 'max-age=63072000'
-        response['Content-Security-Policy'] = 'self'
+        # response['Content-Security-Policy'] = 'self'
 
         # Code to be executed for each request/response after
         # the view is called.
