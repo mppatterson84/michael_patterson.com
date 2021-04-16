be30a65a2e4acf77e2c4ac3f444085eb9b274c5f
mppatterson84 <mppatterson84@gmail.com>
2021-04-15 19:33:58 -0500

    update content security policy http header

--- mysite/middleware.py
+++ mysite/middleware.py
@@ -9,8 +9,31 @@ class ExtraHttpHeaders:
 
         response = self.get_response(request)
         response['Strict-Transport-Security'] = 'max-age=63072000'
-        # response['Content-Security-Policy'] = 'self'
 
+        csp_sse_urls = [
+            "https://stats.g.doubleclick.net",
+            "https://www.google-analytics.com",
+            "https://code.jquery.com",
+            "https://www.googletagmanager.com",
+            "https://cdn.jsdelivr.net",
+            "https://cdnjs.cloudflare.com",
+            "https://www.google.com",
+            "https://www.gstatic.com"
+        ]
+
+        csp_sse = ' '.join([str(u) for u in csp_sse_urls])
+
+        csp = [
+            "default-src 'self';",
+            "img-src *;",
+            f"script-src-elem 'self' {csp_sse};",
+            "style-src * 'unsafe-inline';",
+            "font-src *;",
+            "connect-src 'self' https://www.google-analytics.com https://stats.g.doubleclick.net;",
+            "frame-src 'self' https://www.google.com/"
+        ]
+
+        response['Content-Security-Policy'] = "".join(csp)
         # Code to be executed for each request/response after
         # the view is called.
 
