db8518215fd557a845d2e479060ff84f33b28ccd
mppatterson84 <mppatterson84@gmail.com>
2021-04-29 09:30:24 -0500

    move analytics js

---
+++ mysite/static/js/analytics.js
@@ -0,0 +1,8 @@
+// google analytics
+window.dataLayer = window.dataLayer || []
+function gtag() {
+    dataLayer.push(arguments)
+}
+gtag('js', new Date())
+
+gtag('config', 'G-8T0FQHFQMD')
--- mysite/static/js/base.js
+++ mysite/static/js/base.js
@@ -12,12 +12,3 @@ hamburger.addEventListener('click', () => {
     }
     $('.collapse').collapse('toggle')
 })
-
-// google analytics
-window.dataLayer = window.dataLayer || []
-function gtag() {
-    dataLayer.push(arguments)
-}
-gtag('js', new Date())
-
-gtag('config', 'G-8T0FQHFQMD')
