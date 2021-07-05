88f725bc6b75661bafdf858a4b18fe26a5b621c0
mppatterson84 <mppatterson84@gmail.com>
2021-07-05 11:38:53 -0500

    increase the max length of the image field

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ from blog.utils import get_unique_slug
 class Post(models.Model):
     title = models.CharField(max_length=110)
     body = models.TextField()
-    image = models.ImageField(upload_to='images/', blank=True)
+    image = models.ImageField(upload_to='images/', max_length=250, blank=True)
     author = models.ForeignKey(
         'auth.User', default='auth.User', on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=timezone.now, blank=False)
