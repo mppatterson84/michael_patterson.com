85d54649867b553c89301f62ff12c8d7c20d2011
mppatterson84 <mppatterson84@gmail.com>
2021-08-06 11:17:19 -0500

    create Task model and migrate

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    detail = models.TextField()
+    user = models.ForeignKey(
+        'auth.User', default='auth.User', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now, blank=False)
+    due_by = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
