15f1db6c31adc2719d219846ec55f7ebed8acd93
mppatterson84 <mppatterson84@gmail.com>
2021-08-06 11:50:18 -0500

    create API viewset

---
+++ tasks/api.py
@@ -0,0 +1,11 @@
+from rest_framework import viewsets, permissions
+from tasks.models import Task
+from tasks.serializers import TaskSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+
+    serializer_class = TaskSerializer
