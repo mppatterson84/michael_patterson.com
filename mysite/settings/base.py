6fea9e1fa2dafe55e9d79a69a29a41b2c175e4a2
mppatterson84 <mppatterson84@gmail.com>
2021-08-07 10:28:45 -0500

    add default authentication to settings

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -175,5 +175,9 @@ SESSION_COOKIE_SECURE = True
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
 }
