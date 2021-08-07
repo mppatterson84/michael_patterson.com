a6243a552a60e974ceebc42b1fa60361a0c1de7b
mppatterson84 <mppatterson84@gmail.com>
2021-08-07 11:43:50 -0500

    update to use viewsets

--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,15 @@
-from rest_framework import generics
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
 from tasks.models import Task
 from tasks.permissions import IsUserOrReadOnly
-from tasks.serializers import TaskSerializer
+from tasks.serializers import TaskSerializer, UserSerializer
 
-class TaskList(generics.ListCreateAPIView):
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializer
 
-class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+class TaskViewSet(viewsets.ModelViewSet):
     permission_classes = (IsUserOrReadOnly,)
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer    
