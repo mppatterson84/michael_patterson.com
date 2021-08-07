4bf2da47c68dab0abf6da090ec3fcec3410d36f7
mppatterson84 <mppatterson84@gmail.com>
2021-08-07 11:43:19 -0500

    add user serializer

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from tasks.models import Task
 
@@ -7,3 +8,9 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username',)
