78150b884fed881a63930d8029f893e63a666ba4
mppatterson84 <mppatterson84@gmail.com>
2021-08-07 09:17:02 -0500

    apply custom permissions to TaskDetail

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from tasks.models import Task
+from tasks.permissions import IsUserOrReadOnly
 from tasks.serializers import TaskSerializer
 
 class TaskList(generics.ListCreateAPIView):
@@ -7,5 +8,6 @@ class TaskList(generics.ListCreateAPIView):
     serializer_class = TaskSerializer
 
 class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsUserOrReadOnly,)
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
