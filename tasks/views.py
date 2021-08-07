6bf4c0649f4eade2d9bb265e8708303b21c1c472
mppatterson84 <mppatterson84@gmail.com>
2021-08-07 16:09:45 -0500

    update task view set permissions

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,13 @@
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from tasks.models import Task
-from tasks.permissions import IsUserOrReadOnly
+from tasks.permissions import IsAuthorOrReadOnly
 from tasks.serializers import TaskSerializer, UserSerializer
 
 
 class TaskViewSet(viewsets.ModelViewSet):
-    permission_classes = (IsUserOrReadOnly,)
+    permission_classes = (IsAuthorOrReadOnly, IsAuthenticatedOrReadOnly,)
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
