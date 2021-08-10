e139e27328db508398e1ac881ebe77141ed08846
mppatterson84 <mppatterson84@gmail.com>
2021-08-10 12:03:37 -0500

    add paths for simplejwt

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +30,6 @@ urlpatterns = [
     path('api-auth', include('rest_framework.urls')),
     path('api/tasks/v1/rest-auth/', include('rest_auth.urls')),
     path('api/tasks/v1/rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
