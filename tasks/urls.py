59e68eec9ca37572e0fcfd06131f4447000cf8bb
mppatterson84 <mppatterson84@gmail.com>
2021-08-12 06:42:11 -0500

    add task list path

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
-from tasks.views import UserViewSet, TaskViewSet
+from tasks.views import (
+    TaskViewSet,
+    UserViewSet,
+    TaskListView,
+)
 
 router = SimpleRouter()
-router.register('users', UserViewSet, basename='users')
+router.register('api/tasks/v1/users', UserViewSet, basename='users')
 router.register('api/tasks/v1', TaskViewSet, basename='tasks')
 
-urlpatterns = router.urls
+
+urlpatterns = [
+    path('tasks/', TaskListView.as_view(), name='tasks'),
+] + router.urls
