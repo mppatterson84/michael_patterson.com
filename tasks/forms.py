6cd6645b3f01d7ffd5a2dd82a37eef4d1ebde5b4
mppatterson84 <mppatterson84@gmail.com>
2021-08-12 06:47:21 -0500

    add task form

---
+++ tasks/forms.py
@@ -0,0 +1,44 @@
+from django import forms
+from tasks.models import Task
+
+
+class TaskForm(forms.ModelForm):
+    title = forms.CharField(
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Task Title",
+                "class": "form-control",
+                "maxlength": "100"
+            }
+        )
+    )
+
+    detail = forms.CharField(
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Task Detail",
+                "class": "form-control"
+            }
+        )
+    )
+
+    due_by = forms.DateTimeField(
+        label="Due",
+        required=True,
+        widget=forms.DateTimeInput(
+            attrs={
+                "type": "datetime-local"
+            }
+        )
+    )
+
+    class Meta:
+        model = Task
+        fields = [
+            "title",
+            "detail",
+            "due_by"
+        ]
+
