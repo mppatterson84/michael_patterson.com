4a9d69324a9e11252e92c44773955aa1d129c2a3
mppatterson84 <mppatterson84@gmail.com>
2021-08-12 06:50:50 -0500

    add task update view

--- tasks/views.py
+++ tasks/views.py
@@ -4,7 +4,7 @@ from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from tasks.forms import TaskForm
+from tasks.forms import TaskUpdateForm, TaskForm
 from tasks.models import Task
 from tasks.permissions import IsAuthorOrReadOnly
 from tasks.serializers import TaskSerializer, UserSerializer
@@ -34,4 +34,10 @@ class TaskCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
+    model = Task
+    template_name = 'tasks/task_update.html'
+    form_class = TaskUpdateForm
+    login_url = '/admin/'
 
