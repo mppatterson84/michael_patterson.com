921af244546b8220d7754bd87cf1189e6f6910db
mppatterson84 <mppatterson84@gmail.com>
2021-08-12 06:48:32 -0500

    add task create view

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,13 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from tasks.forms import TaskForm
 from tasks.models import Task
 from tasks.permissions import IsAuthorOrReadOnly
 from tasks.serializers import TaskSerializer, UserSerializer
-
 
 # API Views
 class TaskViewSet(viewsets.ModelViewSet):
@@ -23,4 +24,14 @@ class TaskListView(ListView):
     model = Task
     template_name = 'tasks/task_list.html'
     ordering = ['pk']
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    template_name = 'tasks/task_create.html'
+    form_class = TaskForm
+    login_url = '/admin/'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
