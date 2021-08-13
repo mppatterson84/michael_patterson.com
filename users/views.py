31011d497de01b9f93705b38a8e4f3c025805eb6
mppatterson84 <mppatterson84@gmail.com>
2021-08-13 15:14:09 -0500

    add registration view

--- users/views.py
+++ users/views.py
@@ -1,4 +1,14 @@
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls.base import reverse_lazy
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+from users.forms import BSUserCreationForm
 
 class DashboardView(TemplateView):
     template_name = 'users/dashboard.html'
+
+class RegisterView(SuccessMessageMixin, CreateView):
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('login')
+    form_class = BSUserCreationForm
+    success_message = 'User was created successfully.'
