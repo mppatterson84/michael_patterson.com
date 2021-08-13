b831a4043e87863039c29cf26f001d476813360b
mppatterson84 <mppatterson84@gmail.com>
2021-08-12 20:14:59 -0500

    add logout path

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ from users.forms import BSAuthenticationForm
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('login/', LoginView.as_view(template_name='registration/login.html', authentication_form=BSAuthenticationForm), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
