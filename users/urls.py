e8e74d662064ac009eda553411b3c0adc06d8e2f
mppatterson84 <mppatterson84@gmail.com>
2021-08-13 08:37:22 -0500

    add password reset confirm paths

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from django.urls.conf import include
 from django.contrib.auth.views import (
     LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView,
-    PasswordResetView, PasswordResetDoneView
+    PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView,
+    PasswordResetCompleteView
 )
 from users.forms import (
-    BSAuthenticationForm, BSPasswordChangeForm, BSPasswordResetForm
+    BSAuthenticationForm, BSPasswordChangeForm, BSPasswordResetForm,
+    BSSetPasswordForm
 )
 from users.views import DashboardView
 
@@ -18,4 +20,6 @@ urlpatterns = [
     path('password_change/done/', PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), name='password_change_done'),
     path('password_reset/', PasswordResetView.as_view(template_name='registration/password_reset_form.html', form_class=BSPasswordResetForm), name='password_reset'),
     path('password_reset/done/', PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html', form_class=BSSetPasswordForm), name='password_reset_confirm'),
+    path('reset/done/', PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
 ]
