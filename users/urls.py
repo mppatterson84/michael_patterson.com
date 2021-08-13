1b9b6d431af19c5e5aece6841e3462b33a2e7a3b
mppatterson84 <mppatterson84@gmail.com>
2021-08-13 08:09:59 -0500

    add password reset paths

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.urls.conf import include
 from django.contrib.auth.views import (
-    LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
+    LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView,
+    PasswordResetView, PasswordResetDoneView
 )
 from users.forms import (
-    BSAuthenticationForm, BSPasswordChangeForm
+    BSAuthenticationForm, BSPasswordChangeForm, BSPasswordResetForm
 )
 from users.views import DashboardView
 
@@ -15,4 +16,6 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('password_change/', PasswordChangeView.as_view(template_name='registration/password_change_form.html', form_class=BSPasswordChangeForm), name='password_change'),
     path('password_change/done/', PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), name='password_change_done'),
+    path('password_reset/', PasswordResetView.as_view(template_name='registration/password_reset_form.html', form_class=BSPasswordResetForm), name='password_reset'),
+    path('password_reset/done/', PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
 ]
