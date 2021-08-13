3688a143a47281e9d7f1502e8331c4a743fc3ab0
mppatterson84 <mppatterson84@gmail.com>
2021-08-13 07:07:25 -0500

    add password change form with added Bootstrap classes

--- users/forms.py
+++ users/forms.py
@@ -1,12 +1,32 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UsernameField
+from django.contrib.auth import password_validation
+from django.contrib.auth.forms import (
+    AuthenticationForm,
+    PasswordChangeForm,
+    UsernameField
+)
 
 
 class BSAuthenticationForm(AuthenticationForm):
     """
-    Extend base class with Bootstrap styling.
+    Extend AuthenticationForm base class with Bootstrap styling.
     """
     username = UsernameField(widget=forms.TextInput(attrs={'autofocus': True, 'class': 'form-control'}))
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={'autocomplete': 'current-password', 'class': 'form-control'}),
+    )
+
+class BSPasswordChangeForm(PasswordChangeForm):
+    """
+    Extend PasswordChangeForm base class with Bootstrap styling.
+    """
+    old_password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password', 'autofocus': True, 'class': 'form-control'}),
+    )
+    new_password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'class': 'form-control'}),
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    new_password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'class': 'form-control'}),
     )
