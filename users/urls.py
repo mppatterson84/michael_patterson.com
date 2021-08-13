58c3e754ca19be58555e6742f3cecd25cc897f8b
mppatterson84 <mppatterson84@gmail.com>
2021-08-12 19:20:05 -0500

    add login path

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ from users.forms import BSAuthenticationForm
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
+    path('login/', LoginView.as_view(template_name='registration/login.html', authentication_form=BSAuthenticationForm), name='login'),
 ]
