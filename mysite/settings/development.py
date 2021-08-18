4ccc678f86a12d660c0baf9cd93cf1b6c34c0458
mppatterson84 <mppatterson84@gmail.com>
2021-08-18 16:10:11 -0500

    Update development.py

--- mysite/settings/development.py
+++ mysite/settings/development.py
@@ -16,7 +16,22 @@ SECURE_SSL_REDIRECT = False
 # CSRF_COOKIE_SAMESITE = 'None'
 # CSRF_COOKIE_SECURE = True
 
-CORS_ORIGIN_WHITELIST = (
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'http://localhost:8000'
-)
+    'http://localhost:8000',
+    'http://192.168.1.25:3000',
+    'http://192.168.1.25:8000',
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:8000",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:3000',
+    'http://localhost:8000',
+    'http://192.168.1.25:3000',
+    'http://192.168.1.25:8000',
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:8000",
+]
