63644812b3a98333b00ebc861a12910a13040361
mppatterson84 <mppatterson84@gmail.com>
2021-09-17 21:31:09 -0500

    add todo serializers

---
+++ todo/serializers.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from todo.models import Todo
+
+class TodoSerializer(serializers.ModelSerializer):
+    due_by = serializers.DateTimeField(format="%A, %B %d, %Y, %I:%M %p")
+
+    class Meta:
+        model = Todo
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username',)
