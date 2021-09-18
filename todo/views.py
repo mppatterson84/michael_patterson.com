43b9926d3d328bfdfa1cbdfb8adaf8fc3a2bd367
mppatterson84 <mppatterson84@gmail.com>
2021-09-17 21:31:30 -0500

    add todo API views

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from todo.models import Todo
+from todo.serializers import TodoSerializer, UserSerializer
 
-# Create your views here.
+
+# API Views
+class TodoViewSet(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_authenticated:
+            return Todo.objects.filter(author=user)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_authenticated:
+            return User.objects.filter(username=user)
