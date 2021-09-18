450d3c4d176cf9afab3850b89102b36dd0a1b2e1
mppatterson84 <mppatterson84@gmail.com>
2021-09-17 21:30:41 -0500

    add todo urls

---
+++ todo/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+from todo.views import (
+    TodoViewSet,
+    UserViewSet,
+)
+
+router = SimpleRouter()
+router.register('api/todos/v1/users', UserViewSet, basename='users')
+router.register('api/todos/v1', TodoViewSet, basename='todos')
+
+
+urlpatterns = [
+    path('api/todos/v1/rest-auth/', include('rest_auth.urls')),
+    path('api/todos/v1/rest-auth/registration/', include('rest_auth.registration.urls')),
+] + router.urls
