e2c0be0d380e0fc04233e9df23aff23a753539c9
Mike Patterson <mppatterson84@gmail.com>
2025-03-28 00:45:53 -0500

    make emails read only in the admin

--- sendemail/admin.py
+++ sendemail/admin.py
@@ -4,8 +4,11 @@ from django.contrib import admin
 # Register your models here.
 
 class EmailAdmin(admin.ModelAdmin):
+    # fields = [('from_email', 'created_at'), 'subject', 'message',] # tuple puts fields on the same line
+    fields = ['from_email', 'created_at', 'subject', 'message',]
     list_display = ('subject', 'from_email', 'created_at')
     search_fields = ('subject', 'from_email', 'created_at')
     list_filter = ('created_at',)
+    readonly_fields = ('from_email', 'subject', 'message', 'created_at',)
 
 admin.site.register(Email, EmailAdmin)
