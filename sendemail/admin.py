8b1077dc64c3d94ba4c7bc42a1d2ce96384b4702
Mike Patterson <mppatterson84@gmail.com>
2025-03-27 23:54:14 -0500

    add details to the emails admin

--- sendemail/admin.py
+++ sendemail/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-admin.site.register(Email)
+class EmailAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'from_email', 'created_at')
+    search_fields = ('subject', 'from_email', 'created_at')
+    list_filter = ('created_at',)
+
+admin.site.register(Email, EmailAdmin)
