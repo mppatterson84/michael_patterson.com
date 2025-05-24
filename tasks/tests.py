bb3c4ac2ea517aac91232674ad2635c65fdc7c0a
Mike Patterson <mppatterson84@gmail.com>
2025-05-24 00:38:11 -0500

    Update tests.py

--- tasks/tests.py
+++ tasks/tests.py
@@ -18,7 +18,7 @@ class TaskTests(TestCase):
             title='Task 1 Title',
             detail='Task 1 detail.',
             completed=False,
-            user=cls.testuser1,
+            author=cls.testuser1,
         )
 
     def test_task_content(self):
@@ -26,8 +26,8 @@ class TaskTests(TestCase):
         expected_title = f'{task.title}'
         expected_detail = f'{task.detail}'
         expected_completed = f'{task.completed}'
-        expected_user = f'{task.user}'
+        expected_author = f'{task.author}'
         self.assertEqual(expected_title, 'Task 1 Title')
         self.assertEqual(expected_detail, 'Task 1 detail.')
         self.assertEqual(expected_completed, 'False')
-        self.assertEqual(expected_user, 'testuser1')
+        self.assertEqual(expected_author, 'testuser1')
